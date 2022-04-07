053d24904f9bb497ce89302ba6331f5acd53f898
Dwise09 <onojadavid09@gmail.com>
2022-04-07 16:37:08 -0700

    program 001

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*
+*main -Entry point
+* 
+*return: always 0 (success)
+*
+*/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0)
+;
