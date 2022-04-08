8085222aab75ff27281bab126321fce9947d8874
Dwise09 <onojadavid09@gmail.com>
2022-04-07 19:43:58 -0700

    useful piece of art

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return 1 (Success)
+ */
+{
+	write(1, "and that piece of art is useful\" -Dora Korpar, 2015-09-19\n", 59);
+	return (1)
+}
