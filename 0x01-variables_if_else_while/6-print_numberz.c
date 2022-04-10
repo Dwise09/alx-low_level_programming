2a82955c09beb7069b0cfba5f583ebdcd2b6eb31
Dwise09 <onojadavid09@gmail.com>
2022-04-10 06:38:15 -0700

    numberz less than 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+int number;
+
+for (number = 0; number <= 10; number++)
+{
+putchar (number);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
