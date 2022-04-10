e4c0f82dec68ba741cc9ab46216c4fddcfaae90d
Dwise09 <onojadavid09@gmail.com>
2022-04-10 06:19:16 -0700

    numbers less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+int number;
+
+for (number = 0; number < 10; number++)
+{
+printf("%number\n", number);
+}
+
+return (0);
+
+}
