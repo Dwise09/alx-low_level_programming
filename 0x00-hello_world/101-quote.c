59a1565499f04d0c725b2f62546933e988f53bde
Dwise09 <onojadavid09@gmail.com>
2022-04-11 10:04:54 -0700

    useful art

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always return 1 (Success)
  */
+
+int main(void)
 {
-	write(1, "and that piece of art is useful\" -Dora Korpar, 2015-09-19\n", 59);
-	return (1)
+
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+return (1);
+
 }
