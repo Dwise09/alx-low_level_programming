2d25db13468d8551f4c31c353bec04276eb79f75
Dwise09 <onojadavid09@gmail.com>
2022-04-11 19:39:48 -0700

    useful art

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,7 +10,8 @@
 int main(void)
 {
 
-fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
 
