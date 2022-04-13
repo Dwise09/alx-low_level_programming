5f984fdace001a4867f12db0ad97cdf20c4440dc
Dwise09 <onojadavid09@gmail.com>
2022-04-13 02:13:42 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+
+char c = 'a';
+
+while (c <= 'z')
+{
+_putchar(c);
+c++
+}
+
+_putchar("\n");
+
+}
