2bb1fce3c2687dcbc8a0ced266fc6f6e609cb91e
Dwise09 <onojadavid09@gmail.com>
2022-04-21 00:51:34 -0700

    print string plus new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by new line
+ *
+ * @str: pointer to the string
+ * Return void
+ */
+
+void _puts(char *str)
+{
+
+int i = 0;
+
+while str([i])
+{
+
+_putchar(str[i]);
+i++;
+
+}
+
+_putchar('\n');
+
+}
