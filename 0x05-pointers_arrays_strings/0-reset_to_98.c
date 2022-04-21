6cbe492606276248638b755d8bc49970c473e5c8
Dwise09 <onojadavid09@gmail.com>
2022-04-20 21:32:27 -0700

    point n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset to 98 -update the value *n to 98
+ *@n: the value to modify
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointer array string READMe
