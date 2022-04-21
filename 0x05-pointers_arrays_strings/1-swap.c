689d1ac6991cc82f7a1f8e18c55fc1a82715c1df
Dwise09 <onojadavid09@gmail.com>
2022-04-20 21:55:34 -0700

    point integer b to c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of one to another
+ *
+ * @a: integer first
+ * @b: integer second
+ */
+ void swap_int(int *a. int *b)
+{
+
+c = 0;
+c = *a;
+*a = *b;
+*b = c;
+
+}
