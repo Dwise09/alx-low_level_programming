7f1de6d2510be609d82c9ec8988216cfb69b3443
Dwise09 <onojadavid09@gmail.com>
2022-04-21 11:53:35 -0700

    print different string character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: pointer to the string
+ * Return:  void
+ */
+
+void puts2(char *str)
+{
+
+int a = 0;
+while (str[a] != '\0')
+{
+
+if (a % 2 ==0)
+{
+_putchar(str[a]);
+}
+
+a++;
+
+}
+
+_putchar('\n');
+
+}
