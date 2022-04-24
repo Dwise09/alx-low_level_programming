544b3ea880843bee3da98e019778e1ce67f796ee
Dwise09 <101394519+Dwise09@users.noreply.github.com>
2022-04-24 09:07:26 -0700

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,6 @@ char *cap_string(char *str)
 	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
 	s = 0;
-
 	while (str[s] != '\0')
 	{
 		if (s == 0 && str[s] >= 97 && str[s] <= 122)
