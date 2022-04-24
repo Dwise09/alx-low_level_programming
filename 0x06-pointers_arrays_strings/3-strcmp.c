318c1ba3d566eca20149477878716a0cd70dc6ec
Dwise09 <101394519+Dwise09@users.noreply.github.com>
2022-04-24 08:31:58 -0700

    Create 3-strcmp.c
    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: input parameter
+ * @s2: input parameter
+ *
+ * Return: 0 if equal or b if is different
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0, y = 0;
+
+	while (s1[x] != '\0' && y == 0)
+	{
+		b = s1[x] - s2[x];
+		x++;
+	}
+	return (y);
+}
