1d80012105518fa7bdbc4d9c74d232b7f754e4b5
Dwise09 <101394519+Dwise09@users.noreply.github.com>
2022-05-15 09:48:04 -0700

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,14 @@
-#ifndef FUNC_H
-#define FUNC_H
-#include <stdlib.h>
+#include "function_pointers.h"
 
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
-#endif
+/**
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
