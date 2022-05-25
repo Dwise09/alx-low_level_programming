bcce5e8485492fb39db1451dd4c25e15c6aa3ec2
Dwise09 <101394519+Dwise09@users.noreply.github.com>
2022-05-25 01:57:53 +0100

    Create list.h

---
+++ 0x012-singly_linked_lists/list.h
@@ -0,0 +1,30 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list - node list struct
+ * @str: string value
+ * @len: string length
+ * @next: pointer to next node
+ */
+
+typedef struct list
+{
+	char *str;
+	int len;
+	struct list *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
+
+
