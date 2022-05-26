9976b1fbd3fadae8f5834ffd353ba795e20f6201
Dwise09 <101394519+Dwise09@users.noreply.github.com>
2022-05-26 15:44:57 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t david;
-        david = 0;
+size_t david;
+david = 0;
 
 while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		david++;
-	}
-	return (david);
+{
+	printf("%d\n", h->n);
+	h = h->next;
+	david++;
+}
+
+return (david);
 }
