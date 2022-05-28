9b576f88ba01ffe4759db298b97f9134bfa37eb7
Dwise09 <101394519+Dwise09@users.noreply.github.com>
2022-05-28 02:44:57 +0100

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -6,33 +6,37 @@
  *
  * Return: the address of the node where the loop starts.
  */
+
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *p2;
-	listint_t *prev;
+listint_t *p2;
+listint_t *prev;
 p2 = head;
-	prev = head;
-	while (head && p2 && p2->next)
+prev = head;
+
+while (head && p2 && p2->next)
+{
+	head = head->next;
+	p2 = p2->next->next;
+
+        if (head == p2)
 	{
-		head = head->next;
-		p2 = p2->next->next;
-if (head == p2)
+		head = prev;
+		prev =  p2;
+		while (1)
 		{
-			head = prev;
-			prev =  p2;
-			while (1)
+			p2 = prev;
+			while (p2->next != head && p2->next != prev)
 			{
-				p2 = prev;
-				while (p2->next != head && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == head)
-					break;
-head = head->next;
+				p2 = p2->next;
 			}
-			return (p2->next);
+			if (p2->next == head)
+				break;
+                        head = head->next;
 		}
+		return (p2->next);
 	}
+}
+
 return (NULL);
 }
