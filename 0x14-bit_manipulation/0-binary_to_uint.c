88bcb083e7fffe27eaa78729d167198be43d76e3
Dwise09 <101394519+Dwise09@users.noreply.github.com>
2022-05-31 02:43:14 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int ui;
 int len, base_two;
 
 if (!b)
-	return (0);
+return (0);
 ui = 0;
 
 for (len = 0; b[len] != '\0'; len++)
@@ -23,12 +23,12 @@ for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 {
 	if (b[len] != '0' && b[len] != '1')
 	{
-		return (0);
+	return (0);
 	}
 
 if (b[len] & 1)
 	{
-		ui += base_two;
+	ui += base_two;
 	}
 }
 
